bf4b75ad994e5cb98f554f7fc0152fb398f5acd4
flmngwllm <flmngwllm232@gmail.com>
2023-10-10 23:38:16 -0400

    updating

--- js/canvas1.js
+++ js/canvas1.js
@@ -1,53 +1,34 @@
-var canvas = document.querySelector('canvas')
-    var c = canvas.getContext('2d')
+var canvas = document.querySelector("canvas");
+var c = canvas.getContext("2d");
 
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
+window.addEventListener("resize", function () {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+});
 
-    window.addEventListener("resize", function() {
-        canvas.width = window.innerWidth;
-        canvas.height = window.innerHeight;		
-    });
+var groundHeight = canvas.height * 0.15;
+//coordinates for color gradient
+var backgroundGradient = c.createLinearGradient(0, 0, 0, canvas.height);
+backgroundGradient.addColorStop(0, "skyblue");
+backgroundGradient.addColorStop(1, "white");
 
+function animate() {
+  window.requestAnimationFrame(animate);
+  // background filling
+  c.fillStyle = backgroundGradient;
+  c.fillRect(0, 0, canvas.width, canvas.height);
 
+  c.fillStyle = "#182028";
+  c.fillRect(0, canvas.height - groundHeight, canvas.width, groundHeight);
 
+  // for (var i = 0; i < rain.length; i++) {
+  //     if (rain[i].length <= 0) {
+  //         rain.splice(i, 1);
+  //     }
+  // }
+}
 
-
-
-
-   
-    var groundHeight = canvas.height * 0.15;
-    //coordinates for color gradient
-    var backgroundGradient = c.createLinearGradient(0,0,0, canvas.height);
-    backgroundGradient.addColorStop(0,"skyblue");
-    backgroundGradient.addColorStop(1,"white");
-
-    
-
-
-
-
-    function animate() {
-        window.requestAnimationFrame(animate);
-        // background filling
-        c.fillStyle = backgroundGradient;
-        c.fillRect(0, 0, canvas.width, canvas.height);
-
-   
-
-        c.fillStyle = "#182028";
-        c.fillRect(0, canvas.height - groundHeight, canvas.width, groundHeight);
-
-
-
-        // for (var i = 0; i < rain.length; i++) {
-        //     if (rain[i].length <= 0) {
-        //         rain.splice(i, 1);
-        //     }
-        // }
-
-        
-    }
-
-    animate();
+animate();
--- js/canvas3.js
+++ js/canvas3.js
@@ -1,99 +1,78 @@
-var canvas = document.querySelector('canvas')
-    var c = canvas.getContext('2d')
-    
+var canvas = document.querySelector("canvas");
+var c = canvas.getContext("2d");
 
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;	
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
-
-    window.addEventListener("resize", function() {
-        canvas.width = window.innerWidth;
-        canvas.height = window.innerHeight;			
-    });
-
+window.addEventListener("resize", function () {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+});
 
 /*
   Design
 */
 
 // stars in the background
-    function BackStar() {
-        this.x = Math.random() * canvas.width;
-        this.y = Math.random() * canvas.height;
-        this.radius = Math.random() * 2;
+function BackStar() {
+  this.x = Math.random() * canvas.width;
+  this.y = Math.random() * canvas.height;
+  this.radius = Math.random() * 2;
 
-        this.draw = function() {
-            c.save();
-            c.beginPath();
-            c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+  this.draw = function () {
+    c.save();
+    c.beginPath();
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
 
-            c.shadowColor = '#E3EAEF';
-            c.shadowBlur = (Math.random() * 10) + 10;
-            c.shadowOffsetX = 0;
-            c.shadowOffsetY = 0;
+    c.shadowColor = "#E3EAEF";
+    c.shadowBlur = Math.random() * 10 + 10;
+    c.shadowOffsetX = 0;
+    c.shadowOffsetY = 0;
 
-            //color of stars
-            c.fillStyle = "white";
-            c.fill()
+    //color of stars
+    c.fillStyle = "white";
+    c.fill();
 
-            c.closePath();	
-            c.restore();
-        }
-    }
-
-
+    c.closePath();
+    c.restore();
+  };
+}
 
 // *  Implementation
 
+var groundHeight = canvas.height * 0.15;
+//coordinates for color gradient
+var backgroundGradient = c.createLinearGradient(0, 0, 0, canvas.height);
+backgroundGradient.addColorStop(0, "#171e26");
+backgroundGradient.addColorStop(1, "gray");
 
-    
-    var groundHeight = canvas.height  * 0.15;
-    //coordinates for color gradient
-    var backgroundGradient = c.createLinearGradient(0,0,0, canvas.height);
-    backgroundGradient.addColorStop(0,"#171e26");
-    backgroundGradient.addColorStop(1,"gray");
-    
-    // var moonGradient = c.createLinearGradient()
-   
+// var moonGradient = c.createLinearGradient()
 
-    var backStars = [];
-    for (var i = 0; i < 26; i++) {
-        backStars.push(new BackStar());
+var backStars = [];
+for (var i = 0; i < 26; i++) {
+  backStars.push(new BackStar());
+}
+
+function animate() {
+  window.requestAnimationFrame(animate);
+  // background filling
+  c.fillStyle = backgroundGradient;
+  c.fillRect(0, 0, canvas.width, canvas.height);
+
+  for (var i = 0; i < backStars.length; i++) {
+    backStars[i].draw();
+  }
+
+  c.fillStyle = "#182028";
+  c.fillRect(0, canvas.height - groundHeight, canvas.width, groundHeight);
+
+  for (var i = 0; i < rain.length; i++) {
+    rain[i].update();
+
+    if (rain[i] <= 0) {
+      rain.splice(i, 1);
     }
+  }
+}
 
-
-
-
-
-    function animate() {
-        window.requestAnimationFrame(animate);
-        // background filling
-        c.fillStyle = backgroundGradient;
-        c.fillRect(0, 0, canvas.width, canvas.height);
-
-        for (var i = 0; i < backStars.length; i++) {
-            backStars[i].draw();
-        }
-
-        c.fillStyle = "#182028";
-        c.fillRect(0, canvas.height - groundHeight, canvas.width , groundHeight);
-
-            
-        
-        
-
-       
-        
-        for (var i = 0; i < rain.length; i++) {
-            rain[i].update();
-
-            if (rain[i] <= 0) {
-                rain.splice(i, 1);
-            }
-        }
-
-    }
-
-    animate();
-
-
+animate();
